cb111f8d8eafd23fe5a5dbb28f759fd717702bd2
Aleksandra Baibik <abaibik@gmail.com>
2021-10-31 20:30:59 +0100

    punkt 4

--- first/src/App.js
+++ first/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import MessageList from "./components/MessageList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "./components/Form";
 
 function App() {
@@ -9,6 +9,19 @@ function App() {
   const sendMessage = (message) => {
     setMessageList([...messageList, message]);
   };
+
+  useEffect(() => {
+    if (messageList.length === 0) {
+      return;
+    }
+    const lastMessage = messageList.at(-1);
+    if (lastMessage.author === "Me") {
+      sendMessage({
+        text: "Your message is very important for me",
+        author: "Bot",
+      });
+    }
+  }, [messageList]);
 
   return (
     <div className="App wrapper">
